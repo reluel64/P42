7f80f9316e35450a594c594a82efdc58e6f9df5a
Alexandru-Daniel Margarit <margaritalexandru.95@gmail.com>
2022-04-25 22:56:48 +0300

    remove obsolete definition of apic_reg_t

--- arch/x86_64/h/apic.h
+++ arch/x86_64/h/apic.h
@@ -76,46 +76,6 @@
 #define DIVIDE_CONFIGURATION_REGISTER       (0x83E)
 #define SELF_IPI_REGISTER                   (0x83F)
 #define APIC_REGISTER_END                   (0x8FF)
-#if 0
-typedef struct apic_reg_t
-{
-    uint32_t rsrvd_0    [4];
-    uint32_t rsrvd_1    [4];
-    uint32_t id         [4];
-    uint32_t version    [4];
-    uint32_t rsrvd_2    [4];
-    uint32_t rsrvd_3    [4];
-    uint32_t rsrvd_4    [4];
-    uint32_t rsrvd_5    [4];
-    uint32_t tpr        [4];
-    uint32_t apr        [4];
-    uint32_t ppr        [4];
-    uint32_t eoi        [4];
-    uint32_t rrd        [4];
-    uint32_t ldr        [4];
-    uint32_t dfrr       [4];
-    uint32_t svr        [4];
-    uint32_t isr_1      [32];
-    uint32_t tmr        [32];
-    uint32_t irr        [32];
-    uint32_t esr        [4];
-    uint32_t rsrvd_6    [24];
-    uint32_t lvt_cmci   [4];
-    uint32_t icr        [8];
-    uint32_t lvt_timer  [4];
-    uint32_t lvt_therm  [4];
-    uint32_t lvt_perf   [4];
-    uint32_t lvt_lint0  [4];
-    uint32_t lvt_lint1  [4];
-    uint32_t lvt_err  [4];
-    uint32_t timer_icnt [4];
-    uint32_t timer_ccnt [4];
-    uint32_t rsrvd_7    [20];
-    uint32_t timer_div  [4];
-    uint32_t rsrvd_8    [4];
-
-}__attribute__((packed)) apic_reg_t;
-#endif
 
 typedef struct apic_device_t
 {
