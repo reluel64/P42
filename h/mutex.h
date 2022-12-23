21b96f3bc76c4d2543ffca9c3e315a58596f467d
Alexandru-Daniel <margaritalexandru.95@gmail.com>
2022-12-24 00:00:22 +0200

    added support for mutex_fifo and mutex_prio

--- h/mutex.h
+++ h/mutex.h
@@ -2,16 +2,22 @@
 #define mutexh
 #include <linked_list.h>
 #include <spinlock.h>
+#include <scheduler.h>
 
 #define MUTEX_RECUSRIVE (1 << 0)
-
+#define MUTEX_FIFO      (1 << 1)
+#define MUTEX_PRIO      (1 << 2)
+#define MUTEX_TASK_ORDER (MUTEX_FIFO | MUTEX_PRIO)
+#define MUTEX_LOWEST_PRIO  SCHED_MAX_PRIORITY
 typedef struct mutex_t
 {
     list_head_t pendq;
     spinlock_t lock;
     volatile void *owner;
     volatile uint32_t rlevel;
+    volatile uint32_t owner_prio;
     int opts;
+
 }mutex_t;
 
 mutex_t *mtx_init(mutex_t *mtx, int options);
