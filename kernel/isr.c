356ad03549245bd339ce21cba8d12a9217f32ba6
Alexandru-Daniel <margaritalexandru.95@gmail.com>
2023-01-09 21:20:06 +0200

    cleaned up code

--- kernel/isr.c
+++ kernel/isr.c
@@ -18,16 +18,17 @@ typedef struct isr_list_t
 
 }isr_list_t;
 
+
+
 static isr_list_t handlers[MAX_HANDLERS];
-static list_head_t eoi_handlers;
-static spinlock_t  eoi_lock;
+static isr_list_t eoi;
 static spinlock_t serial_lock;
 
 int isr_init(void)
 {
     memset(&handlers, 0, sizeof(handlers));
-    linked_list_init(&eoi_handlers);
-    spinlock_rw_init(&eoi_lock);
+    linked_list_init(&eoi.head);
+    spinlock_rw_init(&eoi.lock);
     spinlock_init(&serial_lock);
 
     for(int i = 0; i < MAX_HANDLERS; i++)
@@ -43,105 +44,90 @@ isr_t *isr_install
     interrupt_handler_t ih, 
     void *pv, 
     uint16_t index, 
-    uint8_t  eoi,
+    uint8_t  is_eoi,
     isr_t *isr_slot
 )
 {
-    isr_t *intr = NULL;
-    uint8_t int_status = 0;
+    isr_t      *intr      = NULL;
+    uint8_t    int_status = 0;
+    isr_list_t *isr_list  = NULL;
 
-    if(index >= MAX_HANDLERS  && eoi == 0)
+    if((index >= MAX_HANDLERS) && (is_eoi == 0))
+    {
         return(NULL);
-    
+    }
+
     if(isr_slot == NULL)
     {
         intr = kmalloc(sizeof(isr_t));
+
+        if(intr == NULL)
+        {
+            return(NULL);
+        }
+
+        intr->allocated = 1;
     }
     else
     {
         intr = isr_slot;
     }
 
-
-    if(intr == NULL)
-        return(NULL);
-
-    memset(intr, 0, sizeof(isr_t));
-
-    /* If the isr slot is null, then we have allocated memory for it */
-    if(isr_slot == NULL)
-        intr->allocated = 1;
-        
+    memset(intr, 0, sizeof(isr_t));    
 
     intr->ih = ih;
     intr->pv = pv;
 
-    if(!eoi)
-    {   
-        spinlock_write_lock_int(&handlers[index].lock, &int_status);
 
-        linked_list_add_head(&handlers[index].head, &intr->node);
-
-        spinlock_write_unlock_int(&handlers[index].lock, int_status);
+    if(is_eoi)
+    {
+        isr_list = &eoi;
     }
     else
     {
-        spinlock_write_lock_int(&eoi_lock, &int_status);
-
-        linked_list_add_head(&eoi_handlers, &intr->node);
-
-        spinlock_write_unlock_int(&eoi_lock, int_status);
+        isr_list = &handlers[index];
     }
+
+    spinlock_write_lock_int(&isr_list->lock, &int_status);
+
+    linked_list_add_head(&isr_list->head, &intr->node);
+
+    spinlock_write_unlock_int(&isr_list->lock, int_status);
+
     return(intr);
 }
 
 int isr_uninstall
 (
     isr_t *isr,
-    uint8_t eoi
+    uint8_t is_eoi
 )
 {
-    isr_t       *intr      = NULL;
-    uint8_t     int_status = 0;
-    list_node_t *node      = NULL;
-    list_node_t *next_node = NULL;    
+    isr_t       *intr       = NULL;
+    uint8_t     int_status  = 0;
+    list_node_t *node       = NULL;
+    list_node_t *next_node  = NULL;    
     isr_list_t  *isr_lst    = NULL;
-
+    uint16_t    max_index   = 0;
 
     if(isr == NULL)
-        return(-1);
-
-    if(eoi)
     {
-        spinlock_write_lock_int(&eoi_lock, &int_status);
-
-        node = linked_list_first(&eoi_handlers);
-    
-        while(node)
-        {
-            next_node = linked_list_next(node);
-
-            intr = (isr_t*)node;
-            
-            if(intr == isr)
-            {
-                linked_list_remove(&eoi_handlers, node);
-
-                if(intr->allocated)
-                    kfree(intr);
-            }
-
-            node = next_node;
-        }
-        spinlock_write_unlock_int(&eoi_lock, int_status);
-
-        return(0);
+        return(-1);
     }
 
-    for(uint16_t i = 0; i <  MAX_HANDLERS; i++)
+    if(is_eoi)
     {
-        isr_lst = &handlers[i];
-        
+        max_index = 1;
+        isr_lst = &eoi;
+    }
+    else
+    {
+        max_index = MAX_HANDLERS;
+        isr_lst = handlers;
+    }
+
+    for(uint16_t i = 0; i <  max_index; i++)
+    {        
         spinlock_write_lock_int(&isr_lst->lock, &int_status);
         
         node = linked_list_first(&isr_lst->head);
@@ -155,13 +141,20 @@ int isr_uninstall
             if(isr == intr)
             {
                 linked_list_remove(&isr_lst->head, node);
-                kfree(intr);
+                
+                if(intr->allocated)
+                {
+                    kfree(intr);
+                }
             }
 
             node = next_node;
         }
         spinlock_write_unlock_int(&isr_lst->lock, int_status);
+
+        isr_lst++;
     }
+
     return(0);
 }
 
@@ -177,12 +170,12 @@ void isr_dispatcher
     list_node_t       *node      = NULL;
     cpu_t             *cpu       = NULL;
     int               int_status = 0;
-    isr_info_t        inf;
+    isr_info_t        inf = {.cpu_id = 0, .iframe = 0};
     
     if(index >= MAX_HANDLERS)
+    {
         return;
-
-    memset(&inf, 0, sizeof(isr_info_t));
+    }
 
     int_lst = &handlers[index];
 
@@ -200,17 +193,19 @@ void isr_dispatcher
         intr = (isr_t*)node;
 
         if(intr->ih)
+        {
             intr->ih(intr->pv, &inf);
+        }
 
         node = linked_list_next(node);
     }
 
     spinlock_read_unlock(&int_lst->lock);
 
-    spinlock_read_lock(&eoi_lock);
+    spinlock_read_lock(&eoi.lock);
 
     /* Send EOIs */
-    node = linked_list_first(&eoi_handlers);
+    node = linked_list_first(&eoi.head);
 
     while(node)
     {
@@ -222,7 +217,7 @@ void isr_dispatcher
         node = linked_list_next(node);
     }
 
-    spinlock_read_unlock(&eoi_lock);
+    spinlock_read_unlock(&eoi.lock);
 
     /* check if we need to reschedule */
     if(cpu && cpu->sched)
