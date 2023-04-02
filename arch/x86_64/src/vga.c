bac44803f46878e4eac6959edfb1929f60fa806a
Alexandru-Daniel Margarit <margaritalexandru.95@gmail.com>
2023-04-02 15:42:55 +0300

    modified vga for i/o code

--- arch/x86_64/src/vga.c
+++ arch/x86_64/src/vga.c
@@ -3,7 +3,8 @@
 #include <stddef.h>
 #include <utils.h>
 #include <vm.h>
-
+#include <io.h>
+#include <port.h>
 #define FB_PHYS_MEM (0xB8000)
 #define VGA_MAX_ROW (25)
 #define VGA_MAX_COL (80)
@@ -22,6 +23,23 @@ typedef struct vga_t
 
 static vga_t vga;
 
+static size_t vga_write
+(
+    void *fd_data,
+    const void *buf,
+    size_t length
+);
+
+static int vga_open
+(
+    void **fd_data,
+    char *path,
+    int flags,
+    int mode
+);
+
+static int vga_io_register(void);
+
 void vga_init()
 {
     vga.base = (uint16_t*)vm_map(NULL, VM_BASE_AUTO, 
@@ -36,71 +54,108 @@ void vga_init()
     spinlock_init(&vga.lock);
     /* clear the buffer */
     memset(vga.base, 0, FB_LEN);
+    __outb(0x3D4, 0x0A);
+	__outb(0x3D5, 0x20);
+    vga_io_register();
+}
+static void vga_new_line
+(
+    vga_t *vga
+)
+{
+    uint16_t *line      = NULL;
+    uint16_t *next_line = NULL;
+    
+    for(size_t i = 0; i < VGA_MAX_ROW - 1; i++)
+    {
+        line = vga->base + i * VGA_MAX_COL;
+        next_line = vga->base + ((i + 1) * VGA_MAX_COL); 
+
+        memcpy(line, next_line, sizeof(uint16_t) * VGA_MAX_COL);
+    }
+
+    memset(next_line, 0, sizeof(uint16_t) * VGA_MAX_COL);
 }
 
-int vga_print_internal(uint8_t *buf)
+static int vga_open
+(
+    void **fd_data,
+    char *path,
+    int flags,
+    int mode
+)
 {
-    size_t len = 0;
-    uint32_t pos = 0;
-    size_t clear_len = 0;
+    *fd_data = &vga;
 
-    len = strlen(buf);
+    return(0);
+}
+ 
 
-    for(pos = 0; pos < len; pos++)
-    {
-        if(vga.col + vga.row * VGA_MAX_COL >= VGA_MAX_ROW * VGA_MAX_COL)
+static size_t vga_write
+(
+    void *fd_data,
+    const void *buf,
+    size_t length
+)
+{
+    vga_t *vga = NULL;
+    uint16_t character = 0;
+    const char *in_buf = NULL;
+    uint16_t *line = NULL;
+
+    vga = fd_data;
+    in_buf = buf;
+
+    for(int i = 0; i < length; i++)
+    {        
+        /* advance line */
+        if(in_buf[i] == '\n')
         {
-            /* Scroll the text up a line */
-            memcpy(vga.base, &vga.base[VGA_MAX_COL], 
-                   sizeof(uint16_t) * 
-                   (VGA_MAX_COL) * 
-                   (VGA_MAX_ROW - 1));
-
-            /* update the variables */
-            vga.row = VGA_MAX_ROW - 1;
-            vga.col = 0;
-
-            /* clear the last line */
-            memset(&vga.base[vga.col + VGA_MAX_COL * vga.row],
-                   0, 
-                   sizeof(uint16_t) * VGA_MAX_COL);
-        }
-
-        if(buf[pos] == '\n')
-        {
-            vga.row++;
-            vga.col = 0;
+            vga->row++;
+            vga->col = 0;
             continue;
         }
-        /* write to the framebuffer */
-        vga.base[vga.col + VGA_MAX_COL * vga.row] = 
-                    (uint16_t)buf[pos]  | (0x7 << 8);
 
-        /* Next character */
-        vga.col++;
-
-        /* If we reached the end of the line, then
-         * do a new line
-         */ 
-        if(vga.col >= VGA_MAX_COL)
+        /* if we reached the end of the line, advance one line */
+        if(vga->col == VGA_MAX_COL)
         {
-            vga.col = 0;
-            vga.row++;
+            vga->col = 0;
+            vga->row++;
         }
+
+        /* in case where the user added too many new lines, rows could
+         * be way out of bounds. therefore we will advance as needed
+        */
+        while(vga->row > VGA_MAX_ROW - 1)
+        {
+            vga_new_line(vga);
+            vga->row--;
+        }
+
+        line = vga->base + (vga->row * VGA_MAX_COL) + vga->col;
+        character = (uint16_t)in_buf[i]  | (0x7 << 8);
+        line[vga->col] = character;
+
+        vga->col++;
     }
+
+    return(0);
 }
 
 
-void vga_print(uint8_t *buf, uint8_t color, uint64_t len)
+
+static io_entry_t vga_entry = 
 {
-    int int_status = 0;
+    .name       = "vga",
+    .open_func  = vga_open,
+    .write_func = vga_write,
+    .read_func  = NULL,
+    .close_func = NULL
+};
 
-    if(vga.base == NULL)
-        return;
 
-    spinlock_lock(&vga.lock);
 
-    vga_print_internal(buf);
-    
-    spinlock_unlock(&vga.lock);
+static int vga_io_register(void)
+{
+    io_entry_register(&vga_entry);
 }
